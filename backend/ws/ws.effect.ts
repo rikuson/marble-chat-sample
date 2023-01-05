f70533ffb563f5b6d98a05a1bdc89b6ec6c57174
Riku Takeuchi <rikuson@users.noreply.github.com>
2023-01-05 17:47:46 +0800

    feat: Remove response message

--- backend/ws/ws.effect.ts
+++ backend/ws/ws.effect.ts
@@ -1,10 +1,11 @@
 import { matchEvent } from '@marblejs/core';
 import { WsEffect, broadcast } from '@marblejs/websockets';
-import { map } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 export const message$: WsEffect = (event$, { client }) =>
   event$.pipe(
     matchEvent('MESSAGE'),
     broadcast(client, ({ type, payload }) => ({ type, payload })),
-    map(() => ({ type: 'OK' }))
+    mergeMap(() => EMPTY)
   );
